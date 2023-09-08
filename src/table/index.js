c7cf4eb0c605966e1c2736e5fdd0385db5c08cba
volcharikus <volkom.d@gmail.com>
2023-09-08 16:01:45 +0300

    changes

--- src/table/index.js
+++ src/table/index.js
@@ -16,7 +16,7 @@ const columnsInfo = [
 ]
 
 const entriesInfo = [
-	['data 1', 'data 3', 'data 3', 'data 4', 'data 5', 'data 6', 'data 7', 'data 8', 'data 9', 'data 10'],
+	['data 1', 'data 2', 'data 3', 'data 4', 'data 5', 'data 6', 'data 7', 'data 8', 'data 9', 'data 10'],
 	['data 1', 'data 2', 'data 3', 'data 4', 'data 5', 'data 6', 'data 7', 'data 8', 'data 9', 'data 10'],
 	['data 1', 'data 2', 'data 3', 'data 4', 'data 5', 'data 6', 'data 7', 'data 8', 'data 9', 'data 10'],
 	['data 1', 'data 2', 'data 3', 'data 4', 'data 5', 'data 6', 'data 7', 'data 8', 'data 9', 'data 10'],
